5f937cd040bb4bcdd3f25235503b8286208907f6
Dayne <mussinyernar@gmail.com>
2017-03-01 17:44:35 +0600

    desktop mockup ended

---
+++ js/demeu.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	                       
+    $('body').on('click','.modal-back, .modal-close',function(){
+        $('.modal-back, .modal, .thx, .modal-calc').fadeOut(500);
+    });
+    
+    $('body').on('click','.jk',function(){
+        $('.modal-back, .modal').fadeIn(500);
+    });
+    
+    // LINE BLOCK
+    $('.work-box__left__circle, .work-box__right__circle').addClass('wow fadeInDown');
+    $('.work-box__left__line, .work-box__right__line').addClass('wow aniline');
+    
+    $('.work-box__left .work-box__shadow, .work-box__left .work-box__title, .work-box__left .work-box__text').addClass('wow fadeInLeft');
+    $('.work-box__right .work-box__shadow, .work-box__right .work-box__title, .work-box__right .work-box__text').addClass('wow fadeInRight');
+    
+    
+});
+
